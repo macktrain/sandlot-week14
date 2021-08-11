906a7dc59def382e1a587014010cc5311adb850e
macktrain <82425068+macktrain@users.noreply.github.com>
2021-08-11 14:40:11 -0600

    Login and sign up are both working

--- public/js/login.js
+++ public/js/login.js
@@ -1,17 +1,16 @@
-const submitLogIn = async (event) => {
+const loginFormHandler = async (event) => {
   event.preventDefault();
-  
+
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-    console.log ("before fetch");
     const response = await fetch('/api/user/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-    console.log (Response);
+
     if (response.ok) {
       document.location.replace('/');
     } else {
@@ -22,7 +21,7 @@ const submitLogIn = async (event) => {
 
 document
   .querySelector('.login-form')
-  .addEventListener('submit', submitLogIn);
+  .addEventListener('submit', loginFormHandler);
 
 document.getElementById('headerMsg').innerHTML = 'The Tech Blog';
 document.getElementById('loginBtn').style.display = 'none';
