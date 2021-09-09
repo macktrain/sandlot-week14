c8eb1b112c1bf9439265f222b5a380a4a8511e82
macktrain <82425068+macktrain@users.noreply.github.com>
2021-09-09 09:43:54 -0600

    code and test complete

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,11 +63,9 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
-      console.log("YES");
       res.status(204).end();
     });
   } else {
-    console.log("NO");
     res.status(404).end();
   }
 });
